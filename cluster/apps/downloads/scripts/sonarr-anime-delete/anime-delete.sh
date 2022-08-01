2f60bee4ff0ad4566eaa163a3da16c00e584e8ec
Toboshii Nakama <63410334+toboshii@users.noreply.github.com>
2022-08-01 00:37:27 -0500

    fix: update script to support both directories

--- cluster/apps/downloads/scripts/sonarr-anime-delete/anime-delete.sh
+++ cluster/apps/downloads/scripts/sonarr-anime-delete/anime-delete.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-location="/media/Incoming/{Usenet|Torrents}/Series"
+locations=(
+    "/media/Incoming/Usenet/Series"
+    "/media/Incoming/Torrents/Series"
+)
 
-find $location -type f \( -iname "*op[0-9]*" -o -iname "*nced*" -o -iname "*ncop*" -o -iname "*music video*" \) -exec rm -rf {} \;
+find "${locations[@]}" -type f \( -iname "*op[0-9]*" -o -iname "*nced*" -o -iname "*ncop*" -o -iname "*music video*" \) -exec rm -rf {} \;
 
